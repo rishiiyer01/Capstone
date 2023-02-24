c5940d9a716a2ac9e1de8074e10cee1724d3b5f7
rishiiyer01 <79063239+rishiiyer01@users.noreply.github.com>
2023-02-23 23:06:17 -0500

    Update capstoneINIT.m
    changed small things, THIS VERSION DOES NOT HAVE THE COUPLED ACTUATORS

--- capstoneINIT.m
+++ capstoneINIT.m
@@ -5,16 +5,16 @@
 clc; clear; close all;
 % %def constants rn these are all wrong and probably not even right order
 % of mag wise
-I=4;
-leverArm=0.1; %%cp-cg, center of pressure-center of gravity
+I=1.5;
+leverArm=-0.1; %%cp-cg, center of pressure-center of gravity
 CNa=37*pi/180;
 %%putting aerodynamic plant+control in a system of form xdot=Ax+Bu, where
 %%x represents the state
 density=1;
 v=150; %%roughly half the speed of sound, this is the freestream air speed
-A=0.25*pi*(6*25.4)^2;
+Area=0.25*pi*(6*25.4)^2;
 C1=-CNa*leverArm*(0.5*density*v^2)/I;
-C2=-CNa*(leverArm^2)*(0.5*density*v)/I;
+C2=-((CNa*(leverArm^2)*(0.5*density*v)/I)+(0.055*0.1*0.1));
 A=[0 0 1 0; 0 0 0 1; C1 0 C2 0; 0 C1 0 C2]; 
 %%note that damping is basically miniscule compared to spring constant
 %%calling it spring constant because this is basically just a mass spring
@@ -27,7 +27,7 @@ Thrust=94;%N
 %recall that our state vector is [theta1,theta2,thetaDot1,thetaDot2]
 C3=Thrust*leverArmMotor/I;
 %B matrix in the dynamical system
-B=[0 0 0 0; 0 0 0 0; C3 0 0 0; 0 C3 0 0];
+B=[0 0; 0 0; C3 0; 0 C3];
 %%
 Bchopped=[C3 0 0 0; 0 C3 0 0];
 %%%from rishi Jan 16 2022, the B matrix that determines the optimal control
@@ -38,7 +38,7 @@ Bchopped=[C3 0 0 0; 0 C3 0 0];
 %%weight of our actuators on the optimization function, and Q defines the
 %%weight of how close we have to be to the reference (0 angle, 0 angular
 %%velocity) We can talk more on this
-Q=[1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-R=Q;
+Q=[0.1 0 0 0; 0 0.1 0 0; 0 0 0.1 0; 0 0 0 0.1];
+R=[1 0; 0 1];
 %R=Q because i was lazy
 [K1,S1,P1] = lqr(A,B,Q,R);

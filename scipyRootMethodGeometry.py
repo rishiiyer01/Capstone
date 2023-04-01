fefa212372013c4feb4a65b6f2876917e1a6b2d7
rishiiyer01 <iyer.ris@northeastern.edu>
2023-04-01 19:38:56 -0400

    Update scipyRootMethodGeometry.py

--- scipyRootMethodGeometry.py
+++ scipyRootMethodGeometry.py
@@ -6,10 +6,18 @@ a=2.065
 L=3.1259
 x2=2.625
 y20=1.2385
-def f1(R,a,L,x2,y20,theta,s1,s2):
+def f1(R,a,L,x2,y20,theta,s1):
     thetad=theta*np.pi/180
     #s1
     s1part1=(((R**2+a**2)**0.5)*np.sin(thetad) -x2)**2
     s1part2=(((R**2+a**2)**0.5)*np.cos(thetad) -y20+s1)**2
     s1part3=L**2
     return(s1part1+s1part2+s1part3)
+def f2(R,a,L,x2,y20,theta2,s2):
+    thetad=theta2*np.pi/180
+    #s2
+    s2part1=(((R**2+a**2)**0.5)*np.sin(thetad) -x2)**2
+    s2part2=(((R**2+a**2)**0.5)*np.cos(thetad) -y20+s2)**2
+    s2part3=L**2
+    return(s2part1+s2part2+s2part3)
+

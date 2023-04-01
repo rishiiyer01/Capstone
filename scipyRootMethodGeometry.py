7ff0dd8470d5a2d95283bb2c34b124c71183fbf0
rishiiyer01 <iyer.ris@northeastern.edu>
2023-04-01 19:36:19 -0400

    Create scipyRootMethodGeometry.py

---
+++ scipyRootMethodGeometry.py
@@ -0,0 +1,15 @@
+##python script for capstone
+import numpy as np
+import scipy
+R=4.303
+a=2.065
+L=3.1259
+x2=2.625
+y20=1.2385
+def f1(R,a,L,x2,y20,theta,s1,s2):
+    thetad=theta*np.pi/180
+    #s1
+    s1part1=(((R**2+a**2)**0.5)*np.sin(thetad) -x2)**2
+    s1part2=(((R**2+a**2)**0.5)*np.cos(thetad) -y20+s1)**2
+    s1part3=L**2
+    return(s1part1+s1part2+s1part3)
